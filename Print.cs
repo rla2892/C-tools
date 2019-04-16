69069a11cf0ffca585da97073e687d6410e8aa6e
Kim Sangjin <37770080+rla2892@users.noreply.github.com>
2019-04-16 10:20:36 +0900

    Create Print.cs

---
+++ Print.cs
@@ -0,0 +1,12 @@
+//출력1
+public void WriteToConsole(IEnumerable items)
+{
+    foreach (object o in items)
+    {
+        Console.WriteLine(o);
+    }
+}
+
+//출력2 간단
+var list = new List<int>(Enumerable.Range(0, 50));
+list.ForEach(Console.WriteLine);
